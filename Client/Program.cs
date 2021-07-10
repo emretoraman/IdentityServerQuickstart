9c586d32b24fac3b6c22439b44bd94436a44f6b0
Emre Toraman <emretoraman@gmail.com>
2021-07-10 17:10:52 +0300

    Protecting an API using Client Credentials

---
+++ Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _ = args;
+            _ = GetAccessToken();
+            Console.ReadLine();
+        }
+
+        private static async Task GetAccessToken()
+        {
+            HttpClient client = new();
+            DiscoveryDocumentResponse disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            TokenResponse tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            HttpClient apiClient = new();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            HttpResponseMessage response = await apiClient.GetAsync("https://localhost:6001/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+
+            string content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(JArray.Parse(content));
+        }
+    }
+}
--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -23,12 +23,8 @@ namespace IdentityServer
             // uncomment, if you want to add an MVC-based UI
             //services.AddControllersWithViews();
 
-            var builder = services.AddIdentityServer(options =>
-            {
-                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
-                options.EmitStaticAudienceClaim = true;
-            })
-                .AddInMemoryIdentityResources(Config.IdentityResources)
+            var builder = services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients);
 
